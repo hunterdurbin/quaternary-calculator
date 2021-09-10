eec90738e936466b90841eb872869e24ecabc988
Hunter Durbin <hmdurbin@bsu.edu>
2021-09-10 19:16:53 -0400

    Add empty methods: base4To10, base10To4

--- src/main/java/bsu.cs495.quarternaryCalculator/BaseConvertor.java
+++ src/main/java/bsu.cs495.quarternaryCalculator/BaseConvertor.java
@@ -1,4 +1,13 @@
 package bsu.cs495.quarternaryCalculator;
 
 public class BaseConvertor {
+
+    public int base10To4(int base10Number) {
+        return 0;
+    }
+
+    public int base4To10(int base4Number) {
+        return 0;
+    }
+
 }
