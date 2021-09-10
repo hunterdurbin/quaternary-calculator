6199bca0c2fa6883e67c49383e2945808a36319b
EricVester <ericevester@gmail.com>
2021-09-10 19:21:08 -0400

    added Calculator classes

---
+++ src/main/java/bsu.cs495.quarternaryCalculator/CalculatorClass.java
@@ -0,0 +1,31 @@
+package bsu.cs495.quarternaryCalculator;
+
+public class CalculatorClass {
+
+    int multiplication(int number1, int number2){
+
+        return 0;
+    }
+
+    int sum(int number1, int number2){
+
+        return 0;
+    }
+
+    int subtraction(int number1, int number2){
+        return 0;
+    }
+
+    int division(int number1, int number2){
+        return 0;
+    }
+
+    int sqaureRoot(int number1) {
+        return 0;
+    }
+
+    int square(int number1){
+        return 0;
+    }
+
+}
