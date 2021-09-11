b9989f467562dbc0b1f98aa07f9a6abc4881a912
Hunter Durbin <hmdurbin@bsu.edu>
2021-09-11 08:24:37 -0400

    Add body to method base10To4

--- src/main/java/bsu.cs495.quarternaryCalculator/BaseConvertor.java
+++ src/main/java/bsu.cs495.quarternaryCalculator/BaseConvertor.java
@@ -1,13 +1,32 @@
 package bsu.cs495.quarternaryCalculator;
 
+import static java.lang.StrictMath.abs;
+
 public class BaseConvertor {
+    private final int BASE10 = 10;
+    private final int BASE4 = 4;
 
     public int base10To4(int base10Number) {
-        return 0;
+        String quaternaryStr = "";
+        int remainingToAdd = abs(base10Number);
+        boolean negative = isNegative(base10Number);
+        while (remainingToAdd > 0) {
+            int nextDigit = remainingToAdd % BASE4;
+            remainingToAdd = remainingToAdd / BASE4;
+            quaternaryStr = String.format("%d%s", nextDigit, quaternaryStr);
+        }
+        if (negative) {
+            quaternaryStr = String.format("-%s", quaternaryStr);
+        }
+        return Integer.parseInt(quaternaryStr);
     }
 
     public int base4To10(int base4Number) {
         return 0;
+    }
+
+    private boolean isNegative(int number) {
+        return number < 0;
     }
 
 }
