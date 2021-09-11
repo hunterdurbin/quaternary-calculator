316f55a4e473924619f3f67dcefc8acbb730e449
Hunter Durbin <hmdurbin@bsu.edu>
2021-09-11 08:25:18 -0400

    Add tests for methods Base10To4

--- src/test/java/bsu.cs495.quarternaryCalculator/testBaseConvertor.java
+++ src/test/java/bsu.cs495.quarternaryCalculator/testBaseConvertor.java
@@ -1,5 +1,35 @@
 package bsu.cs495.quarternaryCalculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class testBaseConvertor {
 
+    @Test
+    @DisplayName("Test base10(3) == base4(3)")
+    void testBase10To4_1() {
+        BaseConvertor convertor = new BaseConvertor();
+        int actual = convertor.base10To4(3);
+        int expected = 3;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test base10(4) == base4(10)")
+    void testBase10To4_2() {
+        BaseConvertor convertor = new BaseConvertor();
+        int actual = convertor.base10To4(4);
+        int expected = 10;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Test base10(10) == base4(22)")
+    void testBase10To4_3() {
+        BaseConvertor convertor = new BaseConvertor();
+        int actual = convertor.base10To4(10);
+        int expected = 22;
+        Assertions.assertEquals(expected, actual);
+    }
 }
