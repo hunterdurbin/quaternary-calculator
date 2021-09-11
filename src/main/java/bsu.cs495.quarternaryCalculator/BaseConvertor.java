2d2ceea8f128ea36126d17d7bd899dd45753f0aa
EricVester <ericevester@gmail.com>
2021-09-11 10:52:20 -0400

    added by version of base4to10 conversion

--- src/main/java/bsu.cs495.quarternaryCalculator/BaseConvertor.java
+++ src/main/java/bsu.cs495.quarternaryCalculator/BaseConvertor.java
@@ -7,7 +7,45 @@ public class BaseConvertor {
     }
 
     public int base4To10(int base4Number) {
-        return 0;
+        int multiplier;
+        int base10total = 0;
+        
+        if (base4Number > 3333333){
+            //got to look up the error code
+            return 0;
+        }
+        // I know a more optimal way using a hashmap still figuring out the code for it.
+        while(base4Number>0) {
+            if (base4Number > 1000000) {
+                multiplier = base4Number / 1000000;
+                base10total = base10total+(multiplier*4096);
+                base4Number = base4Number - (1000000*multiplier);
+            }else if (base4Number > 100000) {
+                multiplier = base4Number / 100000;
+                base10total = base10total + (multiplier * 1024);
+                base4Number = base4Number - (100000 * multiplier);
+            }else if (base4Number > 10000) {
+                multiplier = base4Number / 10000;
+                base10total = base10total + (multiplier * 256);
+                base4Number = base4Number - (10000 * multiplier);
+            }else if (base4Number > 1000) {
+                multiplier = base4Number / 1000;
+                base10total = base10total + (multiplier * 64);
+                base4Number = base4Number - (1000 * multiplier);
+            }else if (base4Number > 100) {
+                multiplier = base4Number / 100;
+                base10total = base10total + (multiplier * 16);
+                base4Number = base4Number - (100 * multiplier);
+            }else if (base4Number > 10) {
+                multiplier = base4Number / 10;
+                base10total = base10total + (multiplier * 4);
+                base4Number = base4Number - (100000 * multiplier);
+            }else{
+                base10total =base10total+base4Number;
+            }
+        }
+
+        return base10total;
     }
 
 }
