bde985001aa8c4c5576ee4e4b79ff4ec3faba741
Jake Chappell <jfchappell@bsu.edu>
2021-09-12 16:18:11 -0400

    Filled in the Calculator class and made unit tests

--- src/main/java/bsu.cs495.quarternaryCalculator/Calculator.java
+++ src/main/java/bsu.cs495.quarternaryCalculator/Calculator.java
@@ -3,29 +3,48 @@ package bsu.cs495.quarternaryCalculator;
 public class Calculator {
 
     int multiplication(int number1, int number2){
-
-        return 0;
+        BaseConvertor b = new BaseConvertor();
+        int input1 = b.base4To10(number1);
+        int input2 = b.base4To10(number2);
+        int result = (input1 * input2);
+        return b.base10To4(result);
     }
 
     int sum(int number1, int number2){
-
-        return 0;
+        BaseConvertor b = new BaseConvertor();
+        int input1 = b.base4To10(number1);
+        int input2 = b.base4To10(number2);
+        int result = (input1 + input2);
+        return b.base10To4(result);
     }
 
     int subtraction(int number1, int number2){
-        return 0;
+        BaseConvertor b = new BaseConvertor();
+        int input1 = b.base4To10(number1);
+        int input2 = b.base4To10(number2);
+        int result = (input1 - input2);
+        return b.base10To4(result);
     }
 
     int division(int number1, int number2){
-        return 0;
+        BaseConvertor b = new BaseConvertor();
+        int input1 = b.base4To10(number1);
+        int input2 = b.base4To10(number2);
+        int result = (input1 / input2);
+        return b.base10To4(result);
     }
 
-    int sqaureRoot(int number1) {
-        return 0;
+    int squareRoot(int number1) {
+        BaseConvertor b = new BaseConvertor();
+        int input = b.base4To10(number1);
+        int result = (int) Math.sqrt(input);
+        return b.base10To4(result);
     }
 
     int square(int number1){
-        return 0;
+        BaseConvertor b = new BaseConvertor();
+        int input = b.base4To10(number1);
+        int result = (int) Math.pow(input, 2);
+        return b.base10To4(result);
     }
-
 }
