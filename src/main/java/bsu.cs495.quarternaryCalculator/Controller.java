d6113c0732bb8c17c4800aa58dae4a309759d71c
Hunter Durbin <hmdurbin@bsu.edu>
2021-09-14 16:58:03 -0400

    Add methods and tests to Controller

--- src/main/java/bsu.cs495.quarternaryCalculator/Controller.java
+++ src/main/java/bsu.cs495.quarternaryCalculator/Controller.java
@@ -2,14 +2,15 @@ package bsu.cs495.quarternaryCalculator;
 
 public class Controller {
     private Calculator calculator = new Calculator();
-    private Operation operation = Operation.NONE;
+    private Operation operation;
     private int input;
     private int number1;
     private int number2;
     private int answer;
-    private boolean isInput = false;
+    private boolean isInput = false; // Only false after number1 is stored, but before any digit is entered for number2
     private boolean isNumber1 = false;
     private boolean isNumber2 = false;
+    private int baseMode;
 
     public Controller() {
         this.operation = Operation.NONE;
@@ -17,12 +18,149 @@ public class Controller {
         this.number1 = 0;
         this.number2 = 0;
         this.answer = 0;
+        this.baseMode = 4;
+    }
+
+    public void changeModeToBase10() {
+        baseMode = 10;
+    }
+
+    public void changeModeToBase4() {
+        baseMode = 4;
     }
 
     public void appendDigitToInput(int digit) {
+        isInput = true;
+        input = Integer.parseInt(String.format("%d%d", input, digit));
     }
 
-    public String getInput() {
-        return String.valueOf(input);
+    public void deleteRecentDigitInInput() {
+        if (input == 0) {
+            return;
+        }
+
+        String inputStr = String.valueOf(input);
+        if (inputStr.length() == 1) {
+            input = 0;
+            return;
+        }
+        input = Integer.parseInt(inputStr.substring(0, inputStr.length()-1));
+    }
+
+    public void resetMemory() {
+        operation = Operation.NONE;
+        number1 = 0;
+        number2 = 0;
+        answer = 0;
+        isNumber1 = false;
+        isNumber2 = false;
+    }
+
+    public int getInput() {
+        return input;
+    }
+
+    public int getAnswer() {
+        switch (baseMode) {
+            case 10:
+                BaseConvertor convertor = new BaseConvertor();
+                return convertor.base4To10(answer);
+            case 4:
+                return answer;
+        }
+        return answer;
+    }
+
+    public String showAnswer() {
+        return String.valueOf(getAnswer());
+    }
+
+    public String showInput() {
+        return String.valueOf(getInput());
+    }
+
+    private boolean storeInputToNumber1() {
+        if (!isNumber1) {
+            number1 = input;
+            isNumber1 = true;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean storeInputToNumber2() {
+        if (!isNumber2) {
+            number2 = input;
+            isNumber2 = true;
+            return true;
+        }
+        return false;
+    }
+
+    private void clearInput() {
+        input = 0;
+        isInput = false;
+    }
+
+    public void startOperation(Operation operation) {
+        resetMemory();
+        storeInputToNumber1();
+        clearInput();
+        this.operation = operation;
+        switch (operation) {
+            case SQUARE -> {
+                answer = calculator.square(number1);
+                isNumber2 = true;
+            }
+            case SQUARE_ROOT -> {
+                answer = calculator.squareRoot(number1);
+                isNumber2 = true;
+            }
+            default -> answer = 0;
+        }
+    }
+
+    public void equalsInitiate() {
+        if (!isNumber1 || operation.equals(Operation.NONE)) {
+            return;
+        }
+        storeInputToNumber2();
+        switch (operation) {
+            case ADD -> answer = calculator.sum(number1, number2);
+            case SUBTRACT -> answer = calculator.subtraction(number1, number2);
+            case MULTIPLY -> answer = calculator.multiplication(number1, number2);
+            case DIVIDE -> answer = calculator.division(number1, number2);
+            default -> answer = 0;
+        }
+        clearInput();
+    }
+
+
+    public String buildOperation() {
+        if (operation.equals(Operation.SQUARE)) {
+            return String.format("%d^2 =", number1);
+        }
+        if (operation.equals(Operation.SQUARE_ROOT)) {
+            return String.format("sqrt(%d) =", number1);
+        }
+
+        if (!isNumber1 && !isNumber2 && isInput) {
+            return String.format("%d", input);
+        } else if (!isInput && !isNumber2) {
+            return String.format("%d %s", number1, operation.label);
+        } else if (!isNumber2) {
+            return String.format("%d %s %d", number1, operation.label, input);
+        }
+        return String.format("%d %s %d =", number1, operation.label, number2);
+    }
+
+    public String convertBase4ToBase10(String number) {
+        BaseConvertor convertor = new BaseConvertor();
+        return String.valueOf(convertor.base4To10(Integer.parseInt(number)));
+    }
+
+    public String convertBase10ToBase4(String number) {
+        BaseConvertor convertor = new BaseConvertor();
+        return String.valueOf(convertor.base10To4(Integer.parseInt(number)));
     }
 }
--- src/test/java/bsu.cs495.quarternaryCalculator/ControllerTest.java
+++ src/test/java/bsu.cs495.quarternaryCalculator/ControllerTest.java
@@ -9,27 +9,27 @@ public class ControllerTest {
 
     @Test
     @DisplayName("Test that a digit gets input to the current input value")
-    @Disabled("Not Implemented yet")
+//    @Disabled("Not Implemented yet")
     void testAppendDigitToInput_1() {
         Controller controller = new Controller();
         controller.appendDigitToInput(0);
-        Assertions.assertEquals("0", controller.getInput());
+        Assertions.assertEquals(0, controller.getInput());
     }
 
     @Test
     @DisplayName("Test that a digit gets input to the current input value")
-    @Disabled("Not Implemented yet")
+//    @Disabled("Not Implemented yet")
     void testAppendDigitToInput_2() {
         Controller controller = new Controller();
         controller.appendDigitToInput(1);
         controller.appendDigitToInput(0);
         controller.appendDigitToInput(3);
-        Assertions.assertEquals("103", controller.getInput());
+        Assertions.assertEquals(103, controller.getInput());
     }
 
     @Test
     @DisplayName("Test that a digit gets input to the current input value")
-    @Disabled("Not Implemented yet")
+//    @Disabled("Not Implemented yet")
     void testAppendDigitToInput_3() {
         Controller controller = new Controller();
         controller.appendDigitToInput(0);
@@ -38,69 +38,156 @@ public class ControllerTest {
         controller.appendDigitToInput(4);
         controller.appendDigitToInput(0);
         controller.appendDigitToInput(2);
-        Assertions.assertEquals("1402", controller.getInput());
+        Assertions.assertEquals(1402, controller.getInput());
     }
 
     @Test
     void testGetInput() {
         Controller controller = new Controller();
-        Assertions.assertEquals("0", controller.getInput());
+        Assertions.assertEquals(0, controller.getInput());
     }
 
     @Test
-    void testStoreInput() {
+//    @Disabled("Not implemented")
+    void testDeleteRecentDigitInInput_1() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(412);
+        controller.deleteRecentDigitInInput();
+        Assertions.assertEquals(41, controller.getInput());
     }
 
     @Test
-    @Disabled
+//    @Disabled("Not implemented")
+    void testDeleteRecentDigitInInput_2() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(21242);
+        controller.deleteRecentDigitInInput();
+        controller.deleteRecentDigitInInput();
+        controller.deleteRecentDigitInInput();
+        Assertions.assertEquals(21, controller.getInput());
+    }
+
+    @Test
+    @DisplayName("Make sure that input is 0 if too many deletes happen")
+//    @Disabled("Not implemented")
+    void testDeleteRecentDigitInInput_3() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(33);
+        controller.deleteRecentDigitInInput();
+        controller.deleteRecentDigitInInput();
+        controller.deleteRecentDigitInInput();
+        Assertions.assertEquals(0, controller.getInput());
+    }
+
+    @Test
+    void testStartSquare_1() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(2);
+        controller.startOperation(Operation.SQUARE);
+        Assertions.assertEquals(10, controller.getAnswer());
+    }
+
+    @Test
+    void testStartSqrt_1() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(10);
+        controller.startOperation(Operation.SQUARE_ROOT);
+        Assertions.assertEquals(2, controller.getAnswer());
+    }
+
+    @Test
+//    @Disabled
     void testStartAddition_1() {
-        Assertions.fail();
+        Controller controller = new Controller();
+        controller.appendDigitToInput(10);
+        controller.startOperation(Operation.ADD);
+        controller.appendDigitToInput(21);
+        controller.equalsInitiate();
+        Assertions.assertEquals(31, controller.getAnswer());
     }
 
     @Test
-    @Disabled
+//    @Disabled
     void testStartSubtraction_1() {
-        Assertions.fail();
+        Controller controller = new Controller();
+        controller.appendDigitToInput(213);
+        controller.startOperation(Operation.SUBTRACT);
+        controller.appendDigitToInput(103);
+        controller.equalsInitiate();
+        Assertions.assertEquals(110, controller.getAnswer());
     }
 
     @Test
-    @Disabled
+//    @Disabled
     void testStartMultiplication_1() {
-        Assertions.fail();
+        Controller controller = new Controller();
+        controller.appendDigitToInput(10);
+        controller.startOperation(Operation.MULTIPLY);
+        controller.appendDigitToInput(2);
+        controller.equalsInitiate();
+        Assertions.assertEquals(20, controller.getAnswer());
     }
 
     @Test
-    @Disabled
+//    @Disabled
     void testStartDivision_1() {
-        Assertions.fail();
+        Controller controller = new Controller();
+        controller.appendDigitToInput(10);
+        controller.startOperation(Operation.DIVIDE);
+        controller.appendDigitToInput(2);
+        controller.equalsInitiate();
+        Assertions.assertEquals(2, controller.getAnswer());
     }
 
     @Test
     @DisplayName("There is an input, but no numbers set in memory")
-    @Disabled
+//    @Disabled
     void testBuildOperation_1() {
-
+        Controller controller = new Controller();
+        controller.appendDigitToInput(31);
+        Assertions.assertEquals("31", controller.buildOperation());
     }
 
     @Test
-    @DisplayName("There is 1 number, no input, no 2nd number")
-    @Disabled
+    @DisplayName("There is input, but no numbers set in memory")
+//    @Disabled
     void testBuildOperation_2() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(10);
+        Assertions.assertEquals("10", controller.buildOperation());
+    }
 
+    @Test
+    @DisplayName("There is 1 number, no 2nd number, and no input")
+//    @Disabled
+    void testBuildOperation_3() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(11);
+        controller.startOperation(Operation.MULTIPLY);
+        Assertions.assertEquals("11 X", controller.buildOperation());
+    }
+
+    @Test
+    @DisplayName("There is 1 number, and 2nd number, and equals has been pressed")
+//    @Disabled
+    void testBuildOperation_4() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(10);
+        controller.startOperation(Operation.ADD);
+        controller.appendDigitToInput(3);
+        controller.equalsInitiate();
+        Assertions.assertEquals("10 + 3 =", controller.buildOperation());
     }
 
     @Test
     @DisplayName("There is 1 number, is input, no 2nd number")
-    @Disabled
-    void testBuildOperation_3() {
-
-    }
-
-    @Test
-    @DisplayName("There is 1 number, and 2nd number")
-    @Disabled
-    void testBuildOperation_4() {
-
+//    @Disabled
+    void testBuildOperation_5() {
+        Controller controller = new Controller();
+        controller.appendDigitToInput(11);
+        controller.startOperation(Operation.MULTIPLY);
+        controller.appendDigitToInput(2);
+        Assertions.assertEquals("11 X 2", controller.buildOperation());
     }
 }
 
