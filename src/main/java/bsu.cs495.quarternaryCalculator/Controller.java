12481e6e6381677066ec652f7cb9caf7b08da4bb
Hunter Durbin <hmdurbin@bsu.edu>
2021-09-14 14:05:48 -0400

    Add fields, constructor and few methods

--- src/main/java/bsu.cs495.quarternaryCalculator/Controller.java
+++ src/main/java/bsu.cs495.quarternaryCalculator/Controller.java
@@ -1,4 +1,28 @@
 package bsu.cs495.quarternaryCalculator;
 
 public class Controller {
+    private Calculator calculator = new Calculator();
+    private Operation operation = Operation.NONE;
+    private int input;
+    private int number1;
+    private int number2;
+    private int answer;
+    private boolean isInput = false;
+    private boolean isNumber1 = false;
+    private boolean isNumber2 = false;
+
+    public Controller() {
+        this.operation = Operation.NONE;
+        this.input = 0;
+        this.number1 = 0;
+        this.number2 = 0;
+        this.answer = 0;
+    }
+
+    public void appendDigitToInput(int digit) {
+    }
+
+    public String getInput() {
+        return String.valueOf(input);
+    }
 }
