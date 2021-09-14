109f20f9b441c4451f6263b24188d2a025f8ebfe
Hunter Durbin <hmdurbin@bsu.edu>
2021-09-14 13:43:08 -0400

    Add some empty tests to ControllerTest

--- src/test/java/bsu.cs495.quarternaryCalculator/ControllerTest.java
+++ src/test/java/bsu.cs495.quarternaryCalculator/ControllerTest.java
@@ -1,4 +1,110 @@
 package bsu.cs495.quarternaryCalculator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class ControllerTest {
+
+    @Test
+    @DisplayName("Test that a digit gets input to the current input value")
+    @Disabled("Not Implemented yet")
+    void testAppendDigitToInput_1() {
+        Assertions.fail();
+    }
+
+    @Test
+    @DisplayName("Test that a digit gets input to the current input value")
+    @Disabled("Not Implemented yet")
+    void testAppendDigitToInput_2() {
+        Assertions.fail();
+    }
+
+    @Test
+    @Disabled
+    void testGetInput() {
+        Assertions.fail();
+    }
+
+    @Test
+    @Disabled
+    void testStartAddition_1() {
+        Assertions.fail();
+    }
+
+    @Test
+    @Disabled
+    void testStartSubtraction_1() {
+        Assertions.fail();
+    }
+
+    @Test
+    @Disabled
+    void testStartMultiplication_1() {
+        Assertions.fail();
+    }
+
+    @Test
+    @Disabled
+    void testStartDivision_1() {
+        Assertions.fail();
+    }
+
+    @Test
+    @DisplayName("There is an input, but no numbers set in memory")
+    @Disabled
+    void testBuildOperation_1() {
+
+    }
+
+    @Test
+    @DisplayName("There is 1 number, no input, no 2nd number")
+    @Disabled
+    void testBuildOperation_2() {
+
+    }
+
+    @Test
+    @DisplayName("There is 1 number, is input, no 2nd number")
+    @Disabled
+    void testBuildOperation_3() {
+
+    }
+
+    @Test
+    @DisplayName("There is 1 number, and 2nd number")
+    @Disabled
+    void testBuildOperation_4() {
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
